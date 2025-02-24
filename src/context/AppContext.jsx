e7c2f1b7edaf39faf01837dfd5b7b83115ad7fd5
NickHeal21 <nickheal21@gmail.com>
2025-02-24 16:11:51 +0530

    Added some changes in Login page

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,10 +4,11 @@ export const  AppContext = createContext()
 
 const  AppContextProvider = (props) =>{
 
-    const [user,setUser] = useState(false) 
+    const [user,setUser] = useState(null) 
+    const [showLogin,setShowLogin] = useState(false); 
 
     const Value = {
-        user,setUser
+        user,setUser,showLogin,setShowLogin
     }
 
     return (
