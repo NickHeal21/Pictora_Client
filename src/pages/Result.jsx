bbdea987b11e2cc9f87a836c806ce68f170156f8
NickHeal21 <nickheal21@gmail.com>
2025-02-27 16:56:42 +0530

    Added animations to the Header Component

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -4,8 +4,8 @@ import { assets } from '../assets/assets'
 const Result = () => {
 
   const [image,setImage] =  useState(assets.sample_img_1)
-  const [isImageLoaded, setIsImageLoaded] = useState(true)
-  const [loading,setLoading] = useState(true)
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
+  const [loading,setLoading] = useState(false)
   const [input,setInput] = useState('')
   const onSubmitHandler = async (e) => { 
     
