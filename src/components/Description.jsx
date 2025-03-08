8cfc9fd079df6bc84a28e5ccdb9a5b27bc9a7c35
NickHeal21 <nickheal21@gmail.com>
2025-03-08 18:04:41 +0530

    made some little changes in frontend

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion'
 const Description = () => {
   return (
     <motion.div
+      
     initial={{ opacity: 0.2, y: 100 }}
       transition={{ duration: 1 }}
       whileInView={{ opacity: 1 , y: 0 }}
